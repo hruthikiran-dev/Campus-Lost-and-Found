4e16809f0a35aafe4e678fac69b365705fa503bd
Boggarapu Hruthikiran <boggarapuhruthikiran18@gmail.com>
2026-05-16 13:27:57 +0530

    fix: skip dotenv in production

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv');
 const path = require('path');
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const app = express();
 
